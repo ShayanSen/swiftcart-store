c523b3472df87eb174929048fed0fef9fc5d3658
Shayan Sen <ssen18e@gmail.com>
2026-05-25 02:14:23 +0530

    implement admin routes and controllers for product management; add ImageKit authentication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,6 +16,9 @@ import productRouter from "./routes/productRouter";
 import meRouter from "./routes/meRouter";
 import streamRouter from "./routes/streamRouter";
 import chekoutRouter from "./routes/chekoutRouter";
+import adminRouter from "./routes/adminRouter";
+
+
 import { polarWebhookHandler } from "./webhooks/polar";
 import { sentryClerkUserMiddleware } from "./middleware/sentryClerkUser";
 
@@ -47,6 +50,7 @@ app.use("/api/me", meRouter);
 app.use("/api/products", productRouter);
 app.use("/api/stream", streamRouter);
 app.use("/api/checkout", chekoutRouter);
+app.use("/api/admin", adminRouter);
 
 const publicDir = path.join(process.cwd(), "public");
 if (fs.existsSync(publicDir)) {
